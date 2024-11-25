f095e4cdd942bdb9900105014b4aa6f6c86d8043
Thiwanga123 <info.thiwangasri@gmail.om>
2024-11-25 11:46:09 +0530

    added changes

--- app/models/M_guider.php
+++ app/models/M_guider.php
@@ -6,6 +6,28 @@ class Guider{
     public function __construct() {
         $this->db = new Database();
     }
+
+    //retrive the bookings from the bookingTable
+    public function getBookings(){
+        $this->db->query("SELECT COUNT(*) FROM booking");
+        return $this->db->resultSet();
+
+
+    }
+
+    //delete the booking from the bookingTable
+    public function deleteBooking($id){
+        $this->db->query("DELETE FROM booking WHERE booking_id = :booking_id");
+        $this->db->bind(':booking_id', $id);
+
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    
 }
 
 ?>
