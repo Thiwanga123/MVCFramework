b5949274579665eb0f84b7bdeabe3d4397564aa3
Sanju20020307 <157909917+Sanju20020307@users.noreply.github.com>
2024-11-28 14:10:34 +0530

    added some changes

--- app/views/tour_guides/Dashboard.php
+++ app/views/tour_guides/Dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/adminpage/Dashboard.css">
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/adminpage/Orders.css">
     <link rel="stylesheet" href="<?php echo URLROOT;?>/css/Common/sidebarHeader.css">
     
 
