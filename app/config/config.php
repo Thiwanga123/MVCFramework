6d5f9d6c2ec2a5874e88dcb0f4b52fee4c1069a9
Thiwanga123 <info.thiwangasri@gmail.com>
2024-12-18 10:25:09 +0530

    some issues

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,6 @@
     //databse config
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', 'root');
     define('DB_NAME', 'journeybeyond');
 ?>
