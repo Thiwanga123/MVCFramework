b8bfcd1ac899680a49622eb0fe577aac9ae0d2df
Sanju20020307 <157909917+Sanju20020307@users.noreply.github.com>
2025-02-18 17:51:31 +0530

    added new changes and fixed some bugs

--- app/views/users/v_contact.php
+++ app/views/users/v_contact.php
@@ -34,54 +34,36 @@
 
         <main>
            
-        
-        <form action="#"  method="POST">
+        <form action="#" method="POST">
             <div class="profile">
                 <div class="profile-left">
-                
                 </div>
                 
                 <div class="profile-center">
                     <h4>Your Name</h4>
-                    <input type="name" id="name" name="name" value="<?php echo $data['name'];?>">
+                    <input type="text" id="name" name="name" value="<?php echo isset($data['name']) ? htmlspecialchars($data['name']) : ''; ?>">
                     <h4>Your Email</h4>
-                    <input type="email" id="email" name="email" value="<?php echo $data['email'];?>">
+                    <input type="email" id="email" name="email" value="<?php echo isset($data['email']) ? htmlspecialchars($data['email']) : ''; ?>">
                     <h4>Your Telephone Number</h4>
-                    <input type="phone_number" id="phone_number" name="phone_number" value="<?php echo $data['phone_number'];?>">
+                    <input type="text" id="phone_number" name="phone_number" value="<?php echo isset($data['phone_number']) ? htmlspecialchars($data['phone_number']) : ''; ?>">
                     <h4>Your NIC number</h4>
-                    <input type="nic" id="nic" name="nic"value="<?php echo $data['nic'];?>">
+                    <input type="text" id="nic" name="nic" value="<?php echo isset($data['nic']) ? htmlspecialchars($data['nic']) : ''; ?>">
                     <h4>Person You want to Contact</h4>
-                    <input type="nic" id="nic" name="nic"value="<?php echo $data['nic'];?>">
-                    
-                    
+                    <input type="text" id="contact_person" name="contact_person" value="<?php echo isset($data['contact_person']) ? htmlspecialchars($data['contact_person']) : ''; ?>">
                 </div>
 
                 <div class="profile-right">
-                    
-                    
-                    <h4>Service Provider ID (If the Person is a Service Proivder) </h4>
-                    <input type="nic" id="nic" name="nic"value="<?php echo $data['nic'];?>" >
+                    <h4>Service Provider ID (If the Person is a Service Provider)</h4>
+                    <input type="text" id="service_provider_id" name="service_provider_id" value="<?php echo isset($data['service_provider_id']) ? htmlspecialchars($data['service_provider_id']) : ''; ?>">
                     <h4>Message</h4>
-                   
-
-                  <!--get the password from the user-->
-               
-                    <input type="text-area" id="msg" name="msg"   style="background-color: rgb(247, 247, 247);height: 300px;width: 80%;max-width: 400px;border-radius: 30px;padding: 0 20px;border: 1px solid gainsboro;gap: 1rem;" required>
-                    
-                    
+                    <textarea id="msg" name="msg" style="background-color: rgb(247, 247, 247); height: 300px; width: 80%; max-width: 400px; border-radius: 30px; padding: 0 20px; border: 1px solid gainsboro; gap: 1rem;" required><?php echo isset($data['msg']) ? htmlspecialchars($data['msg']) : ''; ?></textarea>
                 </div>
+                
                 <div class="profile-actions">
-                    <input type="Submit"></input>
+                    <input type="submit" value="Submit">
                 </div>
-                </form>
             </div>
-
-
-
-
-
-
-          </main>
+        </form>
 
      </div>
 
