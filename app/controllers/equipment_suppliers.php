d8ec3e5eea48a0bc8b1e5efe64dc8b5bb9bb3f22
Sanju20020307 <157909917+Sanju20020307@users.noreply.github.com>
2025-04-17 02:03:33 +0530

    Add Equipment_Suppliers controller with methods for dashboard, inventory, orders, reviews, bank details, payments, notifications, and profile

---
+++ app/controllers/equipment_suppliers.php
@@ -0,0 +1,113 @@
+<?php
+
+class Equipment_Suppliers extends Controller{
+
+    private $productModel;
+    private $userModel;
+
+    public function __construct(){
+        $this->productModel = $this->model('ProductModel');
+    }
+
+    public function index(){
+        echo "hi";
+    }
+
+    public function dashboard(){
+
+        if (isset($_SESSION['id'])) {
+            $this->view('equipment_supplier/Dashboard');
+        } else {
+            redirect('ServiceProvider');
+        }
+       
+    
+}
+
+
+
+    public function myInventory(){
+
+        if (isset($_SESSION['id'])) {
+
+            $supplierId = $_SESSION['id'];
+
+            $this->productModel = $this->model('ProductModel');
+            $products = $this->productModel->getAllProducts($supplierId);
+            $this->view('equipment_supplier/MyInventory',['products' => $products]);
+
+        } else {
+            redirect('ServiceProvider');
+        }
+
+    }
+
+    public function orders(){
+        
+        if (isset($_SESSION['id'])) {
+            $this->view('equipment_supplier/Orders');
+        } else {
+            redirect('ServiceProvider/login');
+        }
+        
+
+    }
+
+    public function reviews(){
+
+        if (isset($_SESSION['id'])) {
+            $this->view('equipment_supplier/Reviews');
+        } else {
+            redirect('ServiceProvider');
+        }
+    }
+
+    public function bankdetails(){
+
+        if (isset($_SESSION['id'])) {
+            $this->view('equipment_supplier/bankdetails');
+        } else {
+            redirect('ServiceProvider');
+        }
+    }
+
+    public function Mypayments(){
+
+        if (isset($_SESSION['id'])) {
+            $this->view('equipment_supplier/Mypayments');
+        } else {
+            redirect('ServiceProvider');
+        }
+    }
+
+    public function notifications(){
+
+        if (isset($_SESSION['id'])) {
+            $this->view('equipment_supplier/Earnings');
+        } else {
+            redirect('ServiceProvider');
+        }
+        
+    }
+
+    public function profile(){
+
+        if (isset($_SESSION['id'])) {
+            $id = $_SESSION['id'];
+            $type = $_SESSION['type'];
+            $details = $this->getProfileDetails($id,$type);
+            $this->view('equipment_supplier/Myprofile',['details' => $details]);
+        } else {
+            redirect('ServiceProvider');
+        }
+    }
+
+    public function getProfileDetails($id, $type){
+        $this->userModel = $this->model('ServiceProviderModel');
+        $data = $this->userModel->getUserData($id,$type);
+        return $data;
+    }
+} 
+
+
+?>
